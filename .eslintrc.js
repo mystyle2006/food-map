a8328fba670f2ea43fa4d4d298b4b85ae1a6dacc
inno <sayyou0918@gmail.com>
2025-09-17 11:34:11 +0900

    Eslint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: [
-    '@react-native',
-    'plugin:prettier/recommended', // Prettier 설정 추가
-    'plugin:@typescript-eslint/recommended',
-  ],
-  plugins: ['prettier', '@typescript-eslint'],
-  rules: {
-    'prettier/prettier': ['error'],
-  },
+  extends: ['@react-native', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  rules: {},
 };
